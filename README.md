191878832e4ba9c34bf3de33151758e15ac24011
fehiepsi <fehiepsi@gmail.com>
2018-10-21 11:10:23 -0400

    set rng seed, refactor api

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
 # Statistical Rethinking with PyTorch and Pyro
 
-[Statistical Rethinking](https://xcelab.net/rm/statistical-rethinking/) is a great book for applied [Bayesian data analysis](https://en.wikipedia.org/wiki/Bayesian_statistics). The [accompanying code for the book](https://github.com/rmcelreath/rethinking) is written in **R** and **Stan**. It is then [ported to Python language](https://github.com/aloctavodia/Statistical-Rethinking-with-Python-and-PyMC3) using [PyMC3](https://docs.pymc.io/). Recently, [Pyro](http://pyro.ai/) emerges as a universal and flexible Bayesian modeling tool (check out [Pyro tutorial page](http://pyro.ai/examples/)), so to attract statisticians to this new library, I decided to make a *Pyronic* version for the code in this repository. Inspired by the *PyMC3onic* version, I keep the code in this repository as close as possible to the original code in the book.
+[Statistical Rethinking](https://xcelab.net/rm/statistical-rethinking/) is a great book for applied [Bayesian data analysis](https://en.wikipedia.org/wiki/Bayesian_statistics). The [accompanying code for the book](https://github.com/rmcelreath/rethinking) is written in **R** and **Stan**. It is then [ported to Python language](https://github.com/aloctavodia/Statistical-Rethinking-with-Python-and-PyMC3) using [PyMC3](https://docs.pymc.io/). Recently, [Pyro](http://pyro.ai/) emerges as a scalable and flexible Bayesian modeling tool (check out [Pyro tutorial page](http://pyro.ai/examples/)), so to attract statisticians to this new library, I decided to make a *Pyronic* version for the code in this repository. Inspired by the *PyMC3onic* version, I keep the code in this repository as close as possible to the original code in the book.
 
 To say a bit more about **Pyro**, it is a universal [probabilistic programming language](https://en.wikipedia.org/wiki/Probabilistic_programming_language) which is built on top of [PyTorch](https://pytorch.org/), a very popular platform for deep learning. If you are familiar with [numpy](http://www.numpy.org/), the transition from `numpy.array` to `torch.tensor` is rather straightforward (as demonstrated in [this tutorial](https://pytorch.org/tutorials/beginner/blitz/tensor_tutorial.html)).
 
 ## Contents
 
-+ [Preface](http://nbviewer.jupyter.org/github/fehiepsi/rethinking-pyro/blob/master/00_preface.ipynb)
++ [Preface](http://nbviewer.jupyter.org/github/fehiepsi/rethinking-pyro/blob/master/notebooks/00_preface.ipynb)
 
-+ [Chapter 1. The Golem of Prague](http://nbviewer.jupyter.org/fehiepsi/fehiepsi/rethinking-pyro/blob/master/01_the_golem_of_prague.ipynb)
++ [Chapter 1. The Golem of Prague](http://nbviewer.jupyter.org/fehiepsi/fehiepsi/rethinking-pyro/blob/master/notebooks/01_the_golem_of_prague.ipynb)
 
-+ [Chapter 2. Small Worlds and Large Worlds](http://nbviewer.jupyter.org/github/fehiepsi/rethinking-pyro/blob/master/02_small_worlds_and_large_worlds.ipynb)
++ [Chapter 2. Small Worlds and Large Worlds](http://nbviewer.jupyter.org/github/fehiepsi/rethinking-pyro/blob/master/notebooks/02_small_worlds_and_large_worlds.ipynb)
 
-+ [Chapter 3. Sampling the Imaginary](http://nbviewer.jupyter.org/github/fehiepsi/rethinking-pyro/blob/master/03_sampling_the_imaginary.ipynb)
++ [Chapter 3. Sampling the Imaginary](http://nbviewer.jupyter.org/github/fehiepsi/rethinking-pyro/blob/master/notebooks/03_sampling_the_imaginary.ipynb)
 
-+ [Chapter 4. Linear Models](http://nbviewer.jupyter.org/github/fehiepsi/rethinking-pyro/blob/master/04_linear_models.ipynb)
++ [Chapter 4. Linear Models](http://nbviewer.jupyter.org/github/fehiepsi/rethinking-pyro/blob/master/notebooks/04_linear_models.ipynb)
 
-+ [Chapter 5. Multivariate Linear Models](http://nbviewer.jupyter.org/github/fehiepsi/rethinking-pyro/blob/master/05_multivariate_linear_models.ipynb)
++ [Chapter 5. Multivariate Linear Models](http://nbviewer.jupyter.org/github/fehiepsi/rethinking-pyro/blob/master/notebooks/05_multivariate_linear_models.ipynb)
 
 + [Chapter 6. Overfitting, Regularization, and Information Criteria]
 
@@ -36,11 +36,11 @@ To say a bit more about **Pyro**, it is a universal [probabilistic programming l
 
 + [Chapter 14. Missing Data and Other Opportunities]
 
-+ [Chapter 15. Horoscopes](http://nbviewer.jupyter.org/github/fehiepsi/rethinking-pyro/blob/master/15_horoscopes.ipynb)
++ [Chapter 15. Horoscopes](http://nbviewer.jupyter.org/github/fehiepsi/rethinking-pyro/blob/master/notebooks/15_horoscopes.ipynb)
 
 ## Setup
 
-You might use `pip` to install the following packages:
+You might use `pip install -r requirements.txt` to install the following packages:
 + `jupyter` for displaying code, visualizations, text in one place,
 + `pandas` for reading data,
 + `matplotlib` for plotting,
--- notebooks/rethinking.py
+++ notebooks/rethinking.py
@@ -1,3 +1,7 @@
+import logging
+import warnings
+
+import matplotlib.pyplot as plt
 import pandas as pd
 import torch
 
@@ -11,11 +15,11 @@ from pyro.infer import SVI, Trace_ELBO
 
 class LM(object):
 
-    def __init__(self, fit_intercept=True, **kwargs):
-        self.name = kwargs.pop("name", "")
+    def __init__(self, **kwargs):
+        self.name = kwargs.pop("name", "lm")
+        self.fit_intercept = kwargs.pop("fit_intercept", True)
         self.y = kwargs.pop(list(kwargs)[-1])
         self.factors = kwargs
-        self.fit_intercept = fit_intercept
         self._sigma = self.y.std()
         
     def _get_coefs(self):
@@ -54,33 +58,47 @@ class LM(object):
 
 class MAP(object):
 
-    def __init__(self, model, start={}, **kwargs):
+    def __init__(self, name, model, start={}, **kwargs):
         self.model = model
-        self._guide = AutoDelta(model, prefix=kwargs.pop("name", "auto"))
+        self._guide = AutoDelta(model, prefix=name)
         self.kwargs = kwargs
         self._latents = []
         for latent, site in poutine.trace(model).get_trace(**kwargs).iter_stochastic_nodes():
-            if latent in start:
-                pyro.param("{}_{}".format(self._guide.prefix, latent), self.start[latent],
+            if start == "mean":
+                pyro.param("{}_{}".format(self._guide.prefix, latent), site["fn"].mean,
+                           constraint=site["fn"].support)
+            elif latent in start:
+                pyro.param("{}_{}".format(self._guide.prefix, latent), start[latent],
                            constraint=site["fn"].support)
             self._latents.append(latent)
 
     def fit(self, lr=1, num_steps=1000):
         svi = SVI(self.model, self._guide, optim.Adam({"lr": lr}), Trace_ELBO())
         losses = []
-        for _ in range(num_steps):
-            losses.append(svi.step(**self.kwargs))
+        with warnings.catch_warnings(record=True) as w:
+            warnings.filterwarnings("once", "Encountered NaN: loss", UserWarning)
+            for _ in range(num_steps):
+                losses.append(svi.step(**self.kwargs))
+        if w:
+            logging.warning(str(w[-1].message)
+                            + "\n\nThe start values for the parameters were invalid. This "
+                            "could be caused by missing values (NA) in the data or by start "
+                            "values outside the parameter constraints. If there are no NA "
+                            "values in the data, try using explicit start values.")
         return losses
 
-    def coef(self):
+    def coef(self, full=False):
         mean = self._guide.median()
-        return torch.cat([mean[latent].detach().unsqueeze(0) for latent in mean])
+        if full:
+            return torch.cat([mean[latent].detach().unsqueeze(0) for latent in mean])
+        else:
+            return {latent: mean[latent].detach() for latent in mean}
 
     def vcov(self):
         return self._guide.covariance(**self.kwargs)
 
     def precis(self, depth=1, prob=0.89, corr=False, digits=2):
-        packed_mean = self.coef()
+        packed_mean = self.coef(full=True)
         cov = self.vcov()
         packed_std_dev = cov.diag().sqrt()
         left_prob, right_prob = (1 - prob) / 2, (1 + prob) / 2
@@ -89,10 +107,10 @@ class MAP(object):
 
         mean, std_dev, quantile_left, quantile_right = {}, {}, {}, {}
         for i, latent in enumerate(self._latents):
-            mean[latent] = packed_mean[i].numpy()
-            std_dev[latent] = packed_std_dev[i].numpy()
-            quantile_left[latent] = packed_quantiles[0, i].numpy()
-            quantile_right[latent] = packed_quantiles[1, i].numpy()
+            mean[latent] = packed_mean[i].tolist()
+            std_dev[latent] = packed_std_dev[i].tolist()
+            quantile_left[latent] = packed_quantiles[0, i].tolist()
+            quantile_right[latent] = packed_quantiles[1, i].tolist()
 
         precis = pd.DataFrame.from_dict({"Mean": mean, "StdDev": std_dev,
             "{:.1f}%".format(left_prob * 100): quantile_left,
@@ -101,12 +119,12 @@ class MAP(object):
             corr_matrix = cov / (packed_std_dev.unsqueeze(1) * packed_std_dev)
             corr_dict = {}
             for i, latent in enumerate(self._latents):
-                corr_dict[latent] = corr_matrix[:, i].numpy()
+                corr_dict[latent] = corr_matrix[:, i].tolist()
             precis = precis.assign(**corr_dict)
         return precis.astype("float").round(digits)
 
     def extract_samples(self, n=10000):
-        packed_samples = dist.MultivariateNormal(self.coef(), self.vcov()).sample(
+        packed_samples = dist.MultivariateNormal(self.coef(full=True), self.vcov()).sample(
             torch.Size([n]))
         samples = {}
         for i, latent in enumerate(self._latents):
@@ -125,20 +143,51 @@ class MAP(object):
         
     def link(self, n=1000, **kwargs):
         factors_matrix = self._get_factors_matrix(**kwargs)
-        coefs_matrix = dist.MultivariateNormal(self.coef(), self.vcov()).sample(
+        coefs_matrix = dist.MultivariateNormal(self.coef(full=True), self.vcov()).sample(
             torch.Size([n]))
         return coefs_matrix[:, :-1].matmul(factors_matrix)
     
     def sim(self, n=1000, **kwargs):
         factors_matrix = self._get_factors_matrix(**kwargs)
-        coefs_matrix = dist.MultivariateNormal(self.coef(), self.vcov()).sample(
+        coefs_matrix = dist.MultivariateNormal(self.coef(full=True), self.vcov()).sample(
             torch.Size([n]))
         mu = coefs_matrix[:, :-1].matmul(factors_matrix)
         sigma = coefs_matrix[:, -1:]
         return dist.Normal(mu, sigma).sample()
 
+    def plot_precis(self, **kwargs):
+        precis = self.precis(**kwargs)
+        plt.plot(precis["Mean"], precis.index, "ko", fillstyle="none")
+        plt.xlabel("Value")
+        for latent in precis.index:
+            plt.plot(precis.loc[latent, ["5.5%", "94.5%"]], [latent, latent], c="k", lw=1.5)
+        plt.gca().invert_yaxis()
+        plt.axvline(x=0, c="k", lw=1, alpha=0.2)
+        plt.grid(axis="y", linestyle="--")
 
-def HPDI(samples, dim=-1, prob=0.89):
+
+def quantile(samples, probs=[0, 0.25, 0.5, 0.75, 1], dim=-1):
+    probs = probs if isinstance(probs, (list, tuple)) else (probs,)
+    full_size = samples.size(dim)
+    sorted_samples = samples.sort(dim)[0]
+    masses = torch.tensor(probs) * full_size
+    lower_quantiles = sorted_samples.index_select(dim,
+                                                  (masses.floor() - 1).clamp(min=0).long())
+    upper_quantiles = sorted_samples.index_select(dim,
+                                                  (masses.ceil() - 1).clamp(min=0).long())
+    dim = (samples.dim() + dim) if dim < 0 else dim
+    t = masses.frac().reshape((len(probs),) + (-1,) * (samples.dim() - dim - 1))
+    quantiles = t * lower_quantiles + (1 - t) * upper_quantiles
+    if len(probs) == 1:
+        quantiles = quantiles.squeeze(dim)
+    return quantiles
+
+
+def PI(samples, prob=0.89, dim=-1):
+    return quantile(samples, ((1 - prob) / 2., (1 + prob) / 2.), dim)
+
+
+def HPDI(samples, prob=0.89, dim=-1):
     full_size = samples.size(dim)
     mass = int(prob * full_size)
     sorted_samples = samples.sort(dim)[0]
@@ -149,14 +198,13 @@ def HPDI(samples, dim=-1, prob=0.89):
     return torch.gather(sorted_samples, dim, indices)
 
 
-def precis(samples, depth=1, prob=0.89, corr=False, digits=2):
+def precis(post, prob=0.89, digits=2):
     mean, std_dev, hpd_left, hpd_right = {}, {}, {}, {}
-    for i, latent in enumerate(samples):
-        mean[latent] = samples[latent].mean().numpy()
-        std_dev[latent] = samples[latent].std().numpy()
-        hpdi = HPDI(samples[latent], prob)
-        hpd_left[latent] = hpdi[0].numpy()
-        hpd_right[latent] = hpdi[1].numpy()
-    return pd.DataFrame.from_dict({"Mean": mean, "StdDev": std_dev,
-        "|{:.2f}".format(prob): hpd_left,
-        "{:.2f}|".format(prob): hpd_right}).astype("float").round(digits)
+    for latent in post:
+        mean[latent] = post[latent].mean().tolist()
+        std_dev[latent] = post[latent].std().tolist()
+        hpdi = HPDI(post[latent], prob)
+        hpd_left[latent] = hpdi[0].tolist()
+        hpd_right[latent] = hpdi[1].tolist()
+    return pd.DataFrame.from_dict({"Mean": mean, "StdDev": std_dev, "|0.89": hpd_left,
+                                  "0.89|": hpd_right}).astype("float").round(digits)
