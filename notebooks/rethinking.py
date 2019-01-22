7e9ae456bcc979f674869b0b15fd1c80a3a28636
fehiepsi <fehiepsi@gmail.com>
2019-01-23 03:36:26 +0900

    update chapter 6

--- notebooks/rethinking.py
+++ notebooks/rethinking.py
@@ -1,9 +1,12 @@
 import inspect
+import os
 import re
 import warnings
 
 import pandas as pd
+import seaborn as sns
 import torch
+import torch.multiprocessing as mp
 from torch.distributions import transform_to, constraints
 
 import pyro
@@ -14,6 +17,10 @@ from pyro.contrib.autoguide import AutoLaplaceApproximation
 from pyro.infer import TracePosterior, TracePredictive, Trace_ELBO
 from pyro.ops.welford import WelfordCovariance
 
+mp.set_sharing_strategy("file_system")
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+warnings.simplefilter("ignore", FutureWarning)
+
 
 class MAP(TracePosterior):
     def __init__(self, model, num_samples=10000, start={}):
@@ -21,7 +28,6 @@ class MAP(TracePosterior):
         self.model = model
         self.num_samples = num_samples
         self.start = start
-        self._max_tries = 5
 
     def _traces(self, *args, **kwargs):
         pyro.clear_param_store()
@@ -43,10 +49,10 @@ class MAP(TracePosterior):
         for name, node in model_trace.nodes.items():
             if node["type"] == "param":
                 if param_constraints[name] is constraints.positive:
-                    prior[name] = dist.HalfCauchy(2)
+                    prior[name] = dist.HalfCauchy(200)
                 else:
-                    prior[name] = dist.Normal(0, 10)
-                unpacked[name] = pyro.param(name).unconstrained()
+                    prior[name] = dist.Normal(0, 1000)
+                unpacked[name] = pyro.param(name).unconstrained().clone().detach()
             elif name in self.start:
                 unpacked[name] = self.start[name]
             elif node["type"] == "sample" and not node["is_observed"]:
@@ -81,7 +87,7 @@ class MAP(TracePosterior):
     def run(self, *args, **kwargs):
         with warnings.catch_warnings():
             warnings.simplefilter("error")
-            for i in range(self._max_tries):
+            for i in range(5):
                 try:
                     return super(MAP, self).run(*args, **kwargs)
                 except Exception as e:
@@ -90,8 +96,9 @@ class MAP(TracePosterior):
 
 
 def _formula_to_predictors(formula, data):
+    dtype = torch.get_default_dtype()
     y_name, expr_str = formula.split(" ~ ")
-    y_node = {"name": y_name, "value": torch.tensor(data[y_name], dtype=torch.float)}
+    y_node = {"name": y_name, "value": torch.tensor(data[y_name], dtype=dtype)}
     y_node["mean"] = y_node["value"].mean()
 
     fit_intercept = True
@@ -107,14 +114,13 @@ def _formula_to_predictors(formula, data):
                 expr = expr.replace(col, "c{}".format(col_to_num[col]))
             eval_expr = expr.lstrip("I")
             eval_map = {"c{}".format(i): data.iloc[:, i] for i in range(data.shape[1])}
-            predictors[org_expr] = torch.tensor(eval(eval_expr, eval_map), dtype=torch.float)
+            predictors[org_expr] = torch.tensor(eval(eval_expr, eval_map), dtype=dtype)
         elif expr.startswith("C"):
             cat_col = expr[2:-1]
             for cat in data[cat_col].unique():
-                predictors["C(d){}".format(cat)] = torch.tensor(data[cat_col] == cat,
-                                                                dtype=torch.float)
+                predictors["C(d){}".format(cat)] = torch.tensor(data[cat_col] == cat, dtype=dtype)
         elif expr in data.columns:
-            predictors[expr] = torch.tensor(data[expr], dtype=torch.float)
+            predictors[expr] = torch.tensor(data[expr], dtype=dtype)
 
     if fit_intercept:
         predictors["Intercept"] = True
@@ -122,13 +128,13 @@ def _formula_to_predictors(formula, data):
 
 
 class LM(MAP):
-    def __init__(self, formula, data, num_samples=10000, start={}):
+    def __init__(self, formula, data, num_samples=10000, start={}, centering=True):
         self.formula = formula
         self.y_node, self.predictors = _formula_to_predictors(formula, data)
         self._predictor_means = {name: predictor.mean() for name, predictor
                                  in self.predictors.items() if name != "Intercept"}
+        self.centering = centering
         super(LM, self).__init__(self.model, num_samples, start)
-        self._max_tries = 1
 
     def model(self, data=None):
         if data is None:
@@ -143,7 +149,7 @@ class LM(MAP):
 
         for name, predictor in predictors.items():
             coef = pyro.sample(name, dist.Normal(0, 10))
-            if fit_intercept:
+            if fit_intercept and self.centering:
                 # use "centering trick"
                 predictor = predictor - self._predictor_means[name]
             mu = mu + coef * predictor
@@ -152,7 +158,7 @@ class LM(MAP):
             return pyro.sample(y_node["name"], dist.Normal(mu, sigma), obs=y_node["value"])
 
     def _get_centering_constant(self, coefs):
-        center = 0
+        center = torch.tensor(0.)
         for name, predictor_mean in self._predictor_means.items():
             center = center + coefs[name] * predictor_mean
         return center
@@ -188,9 +194,9 @@ def coef(posterior):
     mean = {}
     node_supports = extract_samples(posterior)
     for node, support in node_supports.items():
-        mean[node] = support.mean()
+        mean[node] = support.mean(dim=0)
     # correct `intercept` due to "centering trick"
-    if isinstance(posterior, LM) and "Intercept" in mean:
+    if isinstance(posterior, LM) and "Intercept" in mean and posterior.centering:
         center = posterior._get_centering_constant(mean)
         mean["Intercept"] = mean["Intercept"] - center
     return mean
@@ -203,7 +209,7 @@ def vcov(posterior):
     cov_scheme = WelfordCovariance(diagonal=False)
     for sample in packed_support:
         cov_scheme.update(sample)
-    return cov_scheme.get_covariance()
+    return cov_scheme.get_covariance(regularize=False)
 
 
 def precis(posterior, corr=False, digits=2):
@@ -226,7 +232,7 @@ def precis(posterior, corr=False, digits=2):
                 df.loc["{}[{}]".format(node, i)] = [mean[i].item(), std[i].item(),
                                                     hpdi[0, i].item(), hpdi[1, i].item()]
     # correct `intercept` due to "centering trick"
-    if isinstance(posterior, LM) and "Intercept" in df.index:
+    if isinstance(posterior, LM) and "Intercept" in df.index and posterior.centering:
         center = posterior._get_centering_constant(df["Mean"].to_dict()).item()
         df.loc["Intercept", ["Mean", "|0.89", "0.89|"]] -= center
 
@@ -247,10 +253,10 @@ def link(posterior, data=None, n=1000):
             trace = posterior.exec_traces[idx]
             mu.append(trace.nodes[obs_node]["fn"].mean)
     else:
-        obs_keys = inspect.signature(posterior.model).parameters - data.keys()
-        obs_dict = {key: None for key in obs_keys}
-        predictive = TracePredictive(poutine.lift(posterior.model, lambda: None),
-                                     posterior, n).run(**data, **obs_dict)
+        data = {name: data[name] if name in data else None
+                for name in inspect.signature(posterior.model).parameters}
+        predictive = TracePredictive(poutine.lift(posterior.model, dist.Normal(0, 1)),
+                                     posterior, n).run(**data)
         for trace in predictive.exec_traces:
             mu.append(trace.nodes[obs_node]["fn"].mean)
     return torch.stack(mu).detach()
@@ -265,81 +271,84 @@ def sim(posterior, data=None, n=1000):
             trace = posterior.exec_traces[idx]
             obs.append(trace.nodes[obs_node]["fn"].sample())
     else:
-        obs_keys = inspect.signature(posterior.model).parameters - data.keys()
-        obs_dict = {key: None for key in obs_keys}
-        predictive = TracePredictive(poutine.lift(posterior.model, lambda: None),
-                                     posterior, n).run(**data, **obs_dict)
+        data = {name: data[name] if name in data else None
+                for name in inspect.signature(posterior.model).parameters}
+        predictive = TracePredictive(poutine.lift(posterior.model, dist.Normal(0, 1)),
+                                     posterior, n).run(**data)
         for trace in predictive.exec_traces:
             obs.append(trace.nodes[obs_node]["value"])
     return torch.stack(obs).detach()
 
 
-def compare(*posteriors):
-    WAIC_dict, WAIC_vec = {}, {}
-    for m in models:
-        WAIC = m.WAIC(pointwise=True)
-        WAIC_vec[m.name] = WAIC.pop("WAIC_vec")
-        WAIC_dict[m.name] = {w_name: w.item() for w_name, w in WAIC.items()}
-    df = pd.DataFrame.from_dict(WAIC_dict).T.sort_values(by="WAIC")
-    df["dWAIC"] = df["WAIC"] - df["WAIC"][0]
-    weight = (-0.5 * torch.tensor(df["dWAIC"].values, dtype=torch.float)).exp()
-    df["weight"] = (weight / weight.sum()).tolist()
+def compare(posteriors):
+    post_ics = {}
+    with torch.no_grad():
+        for name in posteriors:
+            post_ics[name] = posteriors[name].information_criterion(pointwise=True)
+    n_cases = post_ics[name]["waic"].size(0)
+    WAIC = {name: post_ics[name]["waic"].sum() for name in posteriors}
+    pWAIC = {name: post_ics[name]["p_waic"].sum() for name in posteriors}
+    SE = {name: (n_cases * post_ics[name]["waic"].var()).sqrt() for name in posteriors}
+    table = pd.DataFrame({"WAIC": WAIC, "pWAIC": pWAIC}).sort_values(by="WAIC")
+    table["dWAIC"] = table["WAIC"] - table.iloc[0, 0]
+    table["weight"] = torch.nn.functional.softmax(-1/2 * torch.tensor(table["dWAIC"]), dim=0)
+    table["SE"] = pd.Series(SE)
     dSE = []
-    for i in range(df.shape[0]):
-        WAIC1 = WAIC_vec[df.index[0]]
-        WAIC2 = WAIC_vec[df.index[i]]
-        dSE.append((WAIC1.size(0) * (WAIC2 - WAIC1).var()).sqrt().item())
-    df["dSE"] = dSE
-    df.rename(columns={"se": "SE"}, inplace=True)
-    return df[["WAIC", "pWAIC", "dWAIC", "weight", "SE", "dSE"]]
+    for i in range(table.shape[0]):
+        WAIC0 = post_ics[table.index[0]]["waic"]
+        WAICi = post_ics[table.index[i]]["waic"]
+        dSE.append((n_cases * (WAICi - WAIC0).var()).sqrt())
+    table["dSE"] = dSE
+    return table.astype(float)
 
 
-def compare_plot(waic_df):
-    plt.plot(waic_df["WAIC"], waic_df.index, "ko", fillstyle="none")
-    for i, model in enumerate(waic_df.index):
-        se_df = waic_df.loc[model, :]
-        plt.plot([se_df["WAIC"] - se_df["SE"], se_df["WAIC"] + se_df["SE"]],
-                 [model, model], c="k")
-        if i > 0:
-            plt.plot([se_df["WAIC"] - se_df["dSE"], se_df["WAIC"] + se_df["dSE"]],
-                     [i - 0.5, i - 0.5], c="gray")
-    plt.plot(waic_df["WAIC"] - 2 * waic_df["pWAIC"], waic_df.index, "ko")
-    plt.plot(waic_df["WAIC"][1:], [i + 0.5 for i in range(waic_df.shape[0] - 1)], "^",
-             c="gray", fillstyle="none")
-    plt.gca().invert_yaxis()
-    plt.axvline(x=waic_df["WAIC"][0], c="k", alpha=0.2)
-    plt.grid(axis="y", linestyle="--")
-    plt.xlabel("deviance")
-    plt.title("WAIC")
+def ensemble(posteriors, data):
+    weighted_num = (compare(posteriors)["weight"] * 1000).astype(int)
+    weighted_num.iloc[-1] -= (sum(weighted_num) - 1000)
+    links = []
+    sims = []
+    for name in weighted_num.index:
+        num_samples = weighted_num[name]
+        links.append(link(posteriors[name], data, num_samples).reshape(num_samples, -1))
+        sims.append(sim(posteriors[name], data, num_samples).reshape(num_samples, -1))
+    num_data = max(l.size(1) for l in links)
+    links = [l.expand(-1, num_data) for l in links]
+    sims = [s.expand(-1, num_data) for s in sims]
+    return {"link": torch.cat(links), "sim": torch.cat(sims)}
 
 
-def coeftab(*models, PI=False):
-    coef_df = pd.concat([m.precis().stack() for m in models], axis=1, sort=False)
-    coef_df.columns = [m.name for m in models]
-    mean_df = coef_df.unstack().xs("Mean", axis=1, level=1)
-    keys = []
-    for m in models:
-        keys += list(m._latent_shapes)
-    keys = list(dict.fromkeys(keys))
-    mean_df = mean_df.loc[keys, :]
-    mean_df.loc["nobs"] = [m.sim(n=1).size(-1) for m in models]
-    if PI:
-        lower_PI = coef_df.unstack().xs("5.5%", axis=1, level=1).loc[keys, :]
-        upper_PI = coef_df.unstack().xs("94.5%", axis=1, level=1).loc[keys, :]
-        return mean_df, lower_PI, upper_PI
-    return mean_df
+def _worker(n, fn, fn_args, child_info=None):
+    if child_info is not None:
+        idx, event, queue = child_info
+        pyro.set_rng_seed(idx)
+    result = []
+    for i in range(n):
+        item = fn(*fn_args)
+        result.append(item)
+        queue.put((idx, item))
+        event.wait()
+        event.clear()
+    return result
 
 
-def ensemble(data, *models):
-    n = int(1e3)
-    shape = (n, list(data.values())[0].size(0))
-    WAIC_df = compare(*models)
-    weight = torch.tensor(WAIC_df["weight"].values, dtype=torch.float)
-    models_dict = {m.name: m for m in models}
-    links = [models_dict[m].link(**data, n=n).reshape(n, -1).expand(shape) for m in WAIC_df.index]
-    sims = [models_dict[m].sim(**data, n=n).reshape(n, -1).expand(shape) for m in WAIC_df.index]
-    index = torch.cat([torch.tensor([0]),
-                       (weight.cumsum(0) * n).round().long().clamp(max=n)])
-    weighted_link = torch.cat([l[index[i]:index[i + 1]] for i, l in enumerate(links)])
-    weighted_sim = torch.cat([s[index[i]:index[i + 1]] for i, s in enumerate(sims)])
-    return {"link": weighted_link, "sim": weighted_sim}
+def replicate(n, fn, fn_args, mc_cores=None):
+    mc_cores = mp.cpu_count() - 1 if mc_cores is None else mc_cores
+    queue = mp.Queue()
+    events = [mp.Event() for i in range(mc_cores)]
+    processes = []
+    for i in range(mc_cores):
+        n_i = n // mc_cores + (i < n % mc_cores)
+        child_info = (i, events[i], queue)
+        p = mp.Process(target=_worker, args=(n_i, fn, fn_args, child_info), daemon=True)
+        p.start()
+        processes.append(p)
+
+    result = []
+    for i in range(n):
+        idx, item = queue.get()
+        result.append(item)
+        events[idx].set()
+
+    for i in range(mc_cores):
+        processes[i].join()
+    return result
